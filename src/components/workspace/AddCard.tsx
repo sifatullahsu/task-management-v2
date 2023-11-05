469b38e2a821f2b0af67bb1fc539f43934e56e97
sifatullahsu <personal.sifat@gmail.com>
2023-11-05 23:43:15 +0600

    Enter functionalities on add task

--- src/components/workspace/AddCard.tsx
+++ src/components/workspace/AddCard.tsx
@@ -6,19 +6,23 @@ import { BsPlusLg } from 'react-icons/bs'
 
 const AddCard = ({ listId }: { listId: string }) => {
   const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [title, setTitle] = useState('')
 
   const [createTask] = useCreateTaskMutation()
 
-  const handleAddList = async () => {
+  const handleAddTask = async () => {
     if (title.length > 0) {
+      setLoading(true)
       const res = await createTask({ data: { title, list: listId } }).unwrap()
 
       if (res.status) {
         toast.success(res.message)
+        setLoading(false)
         setOpen(false)
         setTitle('')
       } else {
+        setLoading(false)
         toast.error(res.message)
       }
     }
@@ -34,12 +38,19 @@ const AddCard = ({ listId }: { listId: string }) => {
             placeholder="Enter a title for this card..."
             className="px-3 py-2 text-sm bg-neutral w-full rounded"
             onChange={e => setTitle(e.target.value)}
+            onKeyDown={e => e.key === 'Enter' && handleAddTask()}
           />
           <div className="space-x-2 mt-2 flex align-middle">
-            <button className="btn btn-primary btn-sm" onClick={handleAddList}>
+            <button className="btn btn-primary btn-sm" onClick={handleAddTask} disabled={loading}>
+              {loading && <span className="loading loading-spinner"></span>}
               Add card
             </button>
-            <button type="button" className="btn btn-ghost btn-sm btn-square" onClick={() => setOpen(false)}>
+            <button
+              type="button"
+              className="btn btn-ghost btn-sm btn-square"
+              onClick={() => setOpen(false)}
+              disabled={loading}
+            >
               <AiOutlineClose />
             </button>
           </div>
