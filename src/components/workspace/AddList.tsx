fb5a5b2d7e1b4b5986c9263a9d0e0311bbe29eaa
sifatullahsu <personal.sifat@gmail.com>
2023-11-05 23:39:56 +0600

    Enter functionalities on add list

--- src/components/workspace/AddList.tsx
+++ src/components/workspace/AddList.tsx
@@ -6,19 +6,23 @@ import { BsPlusLg } from 'react-icons/bs'
 
 const AddList = () => {
   const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [title, setTitle] = useState('')
 
   const [createList] = useCreateListMutation()
 
   const handleAddList = async () => {
     if (title.length > 0) {
+      setLoading(true)
       const res = await createList({ data: { title } }).unwrap()
 
       if (res.status) {
         toast.success(res.message)
+        setLoading(false)
         setOpen(false)
         setTitle('')
       } else {
+        setLoading(false)
         toast.error(res.message)
       }
     }
@@ -34,12 +38,19 @@ const AddList = () => {
             placeholder="Enter list title..."
             className="px-3 py-2 text-sm bg-neutral w-full rounded"
             onChange={e => setTitle(e.target.value)}
+            onKeyDown={e => e.key === 'Enter' && handleAddList()}
           />
           <div className="space-x-2 mt-2 flex align-middle">
-            <button className="btn btn-primary btn-sm" onClick={handleAddList}>
+            <button className="btn btn-primary btn-sm" onClick={handleAddList} disabled={loading}>
+              {loading && <span className="loading loading-spinner"></span>}
               Add list
             </button>
-            <button type="button" className="btn btn-ghost btn-sm btn-square" onClick={() => setOpen(false)}>
+            <button
+              type="button"
+              className="btn btn-ghost btn-sm btn-square"
+              onClick={() => setOpen(false)}
+              disabled={loading}
+            >
               <AiOutlineClose />
             </button>
           </div>
