4283ab2b780ab9ca5c285ef6d9d9c0f0f26c8418
sifatullahsu <personal.sifat@gmail.com>
2023-11-06 14:25:47 +0600

    login info

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import MainLayout from '@/layouts/MainLayout'
 import { NextLayout } from '@/types'
 import { signIn } from 'next-auth/react'
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 type Inputs = {
@@ -10,8 +11,10 @@ type Inputs = {
 
 const LoginPage: NextLayout = () => {
   const { register, handleSubmit } = useForm<Inputs>()
+  const [loading, setLoading] = useState(false)
 
   const onSubmit: SubmitHandler<Inputs> = data => {
+    setLoading(true)
     signIn('credentials', {
       ...data,
       redirect: true,
@@ -40,8 +43,20 @@ const LoginPage: NextLayout = () => {
           />
         </div>
 
-        <button className="btn btn-primary btn-sm mt-5">Login</button>
+        <button className="btn btn-primary btn-sm mt-5" disabled={loading}>
+          {loading && <span className="loading loading-spinner"></span>}
+          Login
+        </button>
       </form>
+      <div>
+        <p className="text-lg font-semibold mt-5">Want to Testing?</p>
+        <div>
+          email: <span className="text-accent">personal.sifat@gmail.com</span>
+        </div>
+        <div>
+          password: <span className="text-accent">12345</span>
+        </div>
+      </div>
     </div>
   )
 }
