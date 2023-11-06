1c74de5554965d1f68ce8109dca8136c0ec0d0c8
sifatullahsu <personal.sifat@gmail.com>
2023-11-06 10:56:43 +0600

    home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="bg-neutral text-neutral-content">
+    <footer className="bg-neutral text-neutral-content footer-center p-4">
       <div className="container">
-        <div className="footer p-10">
-          <nav>
-            <header className="footer-title">Services</header>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
-          </nav>
-          <nav>
-            <header className="footer-title">Company</header>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
-          </nav>
-          <nav>
-            <header className="footer-title">Legal</header>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </nav>
-        </div>
+        <aside>
+          <p>Copyright © 2023 - All right reserved by SIFAT ULLAH</p>
+        </aside>
       </div>
     </footer>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,44 @@
+import hero from '@/assets/hero.png'
 import MainLayout from '@/layouts/MainLayout'
 import { NextLayout } from '@/types'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const HomePage: NextLayout = () => {
+  const { data: session } = useSession()
+
   return (
-    <div>
-      <div>Home Page</div>
+    <div className="hero container px-6 py-8 md:py-16 mx-auto">
+      <div className="items-center lg:flex">
+        <div className="w-full lg:w-1/2">
+          <div className="lg:max-w-lg">
+            <Image className="w-28 h-28 mb-3 block md:hidden" src={hero} alt="" />
+            <h1 className="text-3xl font-bold lg:text-5xl">
+              Task Management
+              <br /> Web App
+            </h1>
+
+            <p className="mt-4">
+              Streamline your workflow, boost collaboration, and meet your project deadlines with ease.
+              Experience the power of efficient task organization and elevate your productivity today.
+            </p>
+
+            {session ? (
+              <Link href="/profile/tasks" className="btn btn-primary btn-sm mt-10">
+                Go To Tasks
+              </Link>
+            ) : (
+              <Link href="/login" className="btn btn-primary btn-sm mt-10">
+                Login Now
+              </Link>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+          <Image className="w-full h-full lg:max-w-3xl px-10 hidden md:block" src={hero} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
